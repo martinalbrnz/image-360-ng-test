864a1aaf5e83fd4adc3403a716b141b0e3784dff
martinalbrnz <martinalbor96@gmail.com>
2023-02-10 14:56:16 -0300

    Add splash images to view picker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,22 @@ import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'panorama/interior',
+    path: 'interior',
     loadComponent: () => import('../app/views/panorama/panorama.component')
     .then(m => m.PanoramaComponent),
   },
   {
-    path: 'panorama/pick',
+    path: 'pick',
     loadComponent: () => import('../app/views/image-picking/image-picking.component')
     .then(m => m.ImagePickingComponent),
   },
   {
-    path: 'panorama/cielo',
+    path: 'cielo',
     loadComponent: () => import('../app/views/cielo/cielo.component')
     .then(m => m.CieloComponent),
   },
   {
-    path: 'panorama/la-favorita',
+    path: 'la-favorita',
     loadComponent: () => import('../app/views/la-favorita/la-favorita.component')
     .then(m => m.LaFavoritaComponent),
   },
